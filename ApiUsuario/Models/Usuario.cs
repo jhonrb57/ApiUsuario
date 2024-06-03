050111aface3291eb8bd0e7f7ffdcd660631088e
jhonrb57 <87083331+jhonrb57@users.noreply.github.com>
2024-06-03 15:58:27 -0500

    Add files via upload

---
+++ ApiUsuario/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ApiUsuario.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public int Cedula { get; set; }
+        public string Telefono { get; set; }
+        public string Direccion { get; set; }
+        public string Email { get; set; }
+    }
+}
